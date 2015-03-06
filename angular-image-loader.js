b4e7f35a062a73af19c4026c071e69d71744bee3
tommyfok <154274174@qq.com>
2015-03-06 16:05:28 +0800

    Update angular-image-loader.js

--- angular-image-loader.js
+++ angular-image-loader.js
@@ -23,6 +23,7 @@ angular.module('tomNgImgLoader', [])
         if (loadedImages.indexOf(imgList[ii]) < 0) {
           if (loadingImages.indexOf(imgList[ii]) < 0) {
             var img;
+            loadingImages.push(imgList[ii]);
 
             try {
               img = new Image();
@@ -40,17 +41,23 @@ angular.module('tomNgImgLoader', [])
                   }
                 }
               };
+              img.src = imgList[ii];
             } else {
-              img.onload = function () {
-                loadedImages.push(imgList[ii]);
-                if (isLoaded(imgList)) {
-                  callback.call(context);
+              // onload is useless when image is in browser cache, so..
+              img.src = imgList[ii];
+              (function checkWidth (_img, _loadedImages, _url, _isLoaded, _imgList, _callback, _context) {
+                if (_img.width > 0 || _img.natrualWidth > 0) {
+                  _loadedImages.push(_url);
+                  if (_isLoaded(_imgList)) {
+                    _callback.call(_context);
+                  }
+                } else {
+                  setTimeout(function () {
+                    checkWidth(_img, _loadedImages, _url, _isLoaded, _imgList, _callback, _context);
+                  }, 200);
                 }
-              };
+              })(img, loadedImages, imgList[ii], isLoaded, imgList, callback, context);
             }
-
-            loadingImages.push(imgList[ii]);
-            img.src = imgList[ii];
           }
         }
       })(i);
